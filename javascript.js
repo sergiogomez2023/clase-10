f6dd373ce875faba74342e04be6b7aec53201274
sergiogomez <139941240+sergiogomez2023@users.noreply.github.com>
2023-08-26 20:45:21 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+var variable1 = "nombre"
+var variable2 = "apellido"
+
+console.log(variable1 +" "+ variable2);
+
+
+var edadAlumno1 = 22
+var edadAlumno2 = 11
+var edadAlumno3 = 10
+
+console.log(edadAlumno1 + edadAlumno2 + edadAlumno3);
+
+
+var edadAlumno1 = 22
+var edadAlumno2 = "doce"
+var edadAlumno3 = 23
+
+console.log(edadAlumno1 +" "+ edadAlumno2);
+
+console.log(edadAlumno1 +" "+ edadAlumno2 +" "+ edadAlumno3);
